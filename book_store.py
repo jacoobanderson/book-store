78f6161c85a1cfd9be244122fd6ad4f3b67a0774
jacoobanderson <j00cwa@gmail.com>
2023-04-04 19:54:19 +0200

    Create registration functionality

--- book_store.py
+++ book_store.py
@@ -26,6 +26,9 @@ def main():
                 menuOption = showMainMenu()
             if menuOption == "1":
                 print(menuOption)
+            if menuOption == "2":
+                createUser(connection)
+                menuOption = 0
             if menuOption == "q":
                 isRunning = False
 
@@ -50,5 +53,36 @@ def showMainMenu():
 
     userInput = input()
     return userInput
+
+def createUser(connection):
+    print("Welcome to the Online Book store\n")
+    print("New Member Registration")
+    
+    firstName = input("Enter first name: ")
+    lastName = input("Enter last name: ")
+    streetAddress = input("Enter street address: ")
+    city = input("Enter city: ")
+    state = input("Enter state: ")
+    zipCode = input("Enter zip: ")
+    phoneNumber = input("Enter phone: ")
+    email = input("Enter email: ")
+    password = input("Enter password: ")
+
+    createMemberQuery = f"""
+     INSERT INTO members
+    (fname, lname, address, city, state, zip, phone, email, password)
+    VALUES
+    ("{firstName}", "{lastName}", "{streetAddress}", "{city}", "{state}",
+    {zipCode}, "{phoneNumber}", "{email}", "{password}")
+    """
+
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(createMemberQuery)
+            connection.commit()
+            print("You have registered successfully!")
+    except Error as e:
+        print("You have not been registered.\n")
+        print(e)
 
 main()
